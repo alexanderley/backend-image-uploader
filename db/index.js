8cb8eb6f8b55890834576cf1453737a984325857
alexanderley <112874444+alexanderley@users.noreply.github.com>
2024-12-08 19:37:52 +0100

    added dynamic database and hide env strings

--- db/index.js
+++ db/index.js
@@ -1,18 +1,12 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
-
 require('dotenv').config()
-console.log(process.env) // remove this after you've confirmed it is working
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
-console.log('yy MONGO 🚕🚕🚕🚕', process.env.MONGODB_URI)
-
-
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project-management-server";
-// const MONGO_URI = "mongodb+srv://alexanderley:S9bmz2e8JjaGifOG@cluster0.6lf1q.mongodb.net/?retryWrites=true&w=majority&";
-// const MONGO_URI = "mongodb+srv://alexanderley:S9bmz2e8JjaGifOG@cluster0.6lf1q.mongodb.net/imageProject?retryWrites=true&w=majority&appName=Cluster0";
+
 
 
 mongoose
